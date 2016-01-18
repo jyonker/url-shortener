65fa1d3ff1d7518dddae738dcffba68ffbca59db
Jonathan Yonker <jonathan.yonker@gmail.com>
2016-01-17 20:58:11 -0800

    Improving header. Adding header to build. Excluding 3rd party code from header grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,19 @@ var redis = require('redis');
 
 //using multiline comments so scss preserves them in the css
 var copyrightBanner =
-`/*Copyright 2016 Jonathan Yonker
+`/* Copyright 2016 Jonathan Yonker
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
+Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-limitations under the License.*/`;
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */`;
 
 module.exports = function (grunt) {
   // load all grunt tasks
@@ -110,7 +110,7 @@ module.exports = function (grunt) {
             'public/scripts/**/*.js',
             'public/styles/**/*.scss',
             'routes/**/*.js',
-            'services/**/*.js',
+            'services/**/!(url-validation-regex).js',
             'app.js',
             'integration.spec.js'
           ]
@@ -141,7 +141,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('ci', ['build', 'shell:failBuildOnGitDiff', 'clearData', 'mochaTest']);
 
-  grunt.registerTask('build', ['clean', 'copy:index', 'useminPrepare', 'sass', 'concat', 'cssmin', 'uglify', 'filerev', 'usemin']);
+  grunt.registerTask('build', ['clean', 'usebanner', 'copy:index', 'useminPrepare', 'sass', 'concat', 'cssmin', 'uglify', 'filerev', 'usemin']);
 
   grunt.task.registerTask('serve', '', function(environment) {
     var tasks = ['startRedis', 'shell:herokuLocal', 'stopRedis'];
